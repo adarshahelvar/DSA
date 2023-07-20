2fe19c5ac96eec6d4d50ec997a6db2442ef2e4e0
Adarsha A <adarshaadi1997@gmail.com>
2023-07-20 18:13:44 +0530

    Array question1

---
+++ Array/Question1.js
@@ -0,0 +1,7 @@
+// Write a program to declare an array of size n and store the numbers 1,2,3,4 ...n in the array and return the array.
+let arr = [];
+let n=10
+for (let i = 0; i < n; i++){
+    arr.push(i)
+}
+console.log(arr);
