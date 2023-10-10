14f91239f815897b8c9e94c23a3ca9c2bcbf1c36
Adarsha A <adarshaadi1997@gmail.com>
2023-10-10 15:42:39 +0530

    Missing element in array : leetcode 2009

---
+++ Leetcode Diff/missingNumberInArray.js
@@ -0,0 +1,36 @@
+/*
+Given an array of numbers between 1 to 9, it is an unsorted array.
+Find the missing number when it is sorted and if the number is missing return 1 else return 0.
+
+Example 1:
+Input: nums = [4,2,5,3]
+Output: 0
+Explanation: nums is already continuous.
+
+Example 2:
+Input: nums = [1,2,3,5,6]
+Output: 1
+Explanation: 4 is missing in series, One possible solution is to change the last element to 4.
+The resulting array is [1,2,3,5,4], which is continuous.
+*/
+
+let nums = [1, 2, 3,4];
+let fun = () => {
+  let sortedElms = nums.sort();
+  let ans;
+
+  for (let i = 0; i < sortedElms.length; i++) {
+    if (sortedElms[i + 1] === sortedElms[i] + 1) {
+      continue;
+    } else {
+      ans = sortedElms[i + 1];
+      break;
+    }
+  }
+  if (ans === undefined) {
+    return 0;
+  } else {
+    return 1;
+  }
+};
+console.log(fun());
